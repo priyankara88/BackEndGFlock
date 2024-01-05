aa49272eddef3df4f32851f6c7187f019668a0d5
priyankara senarathne <priyankara.sliit@gmail.com>
2024-01-05 22:26:10 +0100

    Update dependencies and fix URL import

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
 const app = express();
-const port = 3000; // You can use any port you prefer
+const port = process.env.PORT || 3000;
+dotenv.config();
 
+app.use(morgan("dev"));
 // Define a basic route
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successfull!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Start the server
 app.listen(port, () => {
